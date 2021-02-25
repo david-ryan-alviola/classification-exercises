f5709bac3eaa2b6676f9e1938ea4c4eb352c5981
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2021-02-25 16:54:07 -0600

    Updated after review and completed exercises 1-3 for exploratory analysis exercises.

--- prepare.py
+++ prepare.py
@@ -3,7 +3,7 @@ import pandas as pd
 def prep_iris(raw_iris_df):
     iris_df = raw_iris_df.copy()
     
-    iris_df.drop(columns=['species_id', 'measurement_id'], inplace=True)
+    iris_df.drop(columns=['Unnamed: 0', 'species_id', 'measurement_id'], inplace=True)
     iris_df.rename(columns={'species_name' : 'species'}, inplace=True)
     
     species_dummies_df = pd.get_dummies(iris_df[['species']], dummy_na=False, drop_first=True)
